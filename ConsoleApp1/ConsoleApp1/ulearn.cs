00c7e0c857b1c75206921770ddc525121b4837ed
Aleksey <aleks-sh@mail.ru>
2019-07-02 23:55:15 +0300

    route_planing

---
+++ ConsoleApp1/ConsoleApp1/ulearn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Ulearn
+    {
+        public static void MakePermutations(int[] permutation, int position)
+        {
+            if (position == permutation.Length)
+            {
+                foreach (var element in permutation)
+                {
+                    Console.Write(element);
+                    Console.WriteLine(" ");
+                    return;
+                }
+            }
+            for (int i = 0; i < permutation.Length; i++)
+            {
+                bool found = false;
+                for(int j=0;j<permutation.Length;j++)
+                    if (permutation[j] == i)
+                    {
+                        found = true;
+                        break;
+                    }
+                if (found) continue;
+                permutation[position] = i;
+                MakePermutations(permutation, position + 1);
+            }
+        }
+
+        
+
+
+    }
+}
