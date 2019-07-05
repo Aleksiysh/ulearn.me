fa7d7a37944d1b34f3b6af3d07ad8b7755759883
Aleksey <aleks-sh@mail.ru>
2019-07-05 12:37:27 +0300

    All Tests OK

--- route_planning/PathFinderTask.cs
+++ route_planning/PathFinderTask.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
+
 
 namespace RoutePlanning
 {
@@ -10,7 +9,7 @@ namespace RoutePlanning
         public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
         {
             var bestOrder = MakeTrivialPermutation(checkpoints);
-            
+
             return bestOrder;
         }
 
@@ -20,43 +19,24 @@ namespace RoutePlanning
             var bestOrder = new int[size];
             for (int i = 0; i < bestOrder.Length; i++)
                 bestOrder[i] = i;
-            double pathBestOrder = getPathOrder(bestOrder, checkpoints);
+            double pathBestOrder = getPathOrder(bestOrder, checkpoints, bestOrder.Length);
             MakePermutations(new int[size], 1, checkpoints, bestOrder);
             return bestOrder;
         }
 
-        private static double getPathOrder(int[] order, Point[] checkpoints)
+        public static void MakePermutations(int[] permutation,
+            int position, Point[] checkpoints, int[] bestOrder)
         {
-            double pathOrder = 0;
-            for(int i = 1; i < order.Length;i++) {
-                pathOrder += getDistance(checkpoints[order[i-1]], checkpoints[order[i]]);
-            }
-
-            return pathOrder;
-
-        }
-
-        public static void MakePermutations(int[] permutation, int position, Point[] checkpoints, int[] bestOrder)
-        {
-            // база рекурсии: позиция равна длине перестановки, достигнут последний элемент
             if (position == permutation.Length)
             {
-                double tmpPath = getPathOrder(permutation, checkpoints);
-                if (getPathOrder(permutation, checkpoints) < getPathOrder(bestOrder, checkpoints))
-                {
-                    //pathBestOrder = tmpPath;
-                    for (int i = 0; i < permutation.Length; i++)
-                        bestOrder[i] = permutation[i];
-                };
-                return ;
+                CheckPermutation(permutation, bestOrder, checkpoints);
+                return;
             }
-            //по очереди ставим все элементы на текущую позицию,
-            //если их еще нет в существующей перестановки в позициях от 0 до текущей
+
+            double bestPath = getPathOrder(bestOrder, checkpoints, bestOrder.Length);
             for (int i = 0; i < permutation.Length; i++)
             {
                 bool found = false;
-                //если текущий элемент найден в перестановке в позиции от 0 до текущей,
-                //работаем со сл элементом
                 for (int j = 0; j < position; j++)
                     if (permutation[j] == i)
                     {
@@ -64,16 +44,37 @@ namespace RoutePlanning
                         break;
                     }
                 if (found) continue;
-                //если елемента нет в перестановке, ставим его на текущее место
                 permutation[position] = i;
-                //вызов для сл позиции
-                MakePermutations(permutation, position + 1, checkpoints, bestOrder);//,pathBestOrder);
-            }           
+                if (getPathOrder(permutation, checkpoints, position) > bestPath)
+                    continue;
+                MakePermutations(permutation, position + 1, checkpoints, bestOrder);
+            }
+        }
+
+        private static double getPathOrder(int[] order, Point[] checkpoints, int end)
+        {
+            double pathOrder = 0;
+            for (int i = 1; i < end; i++)
+            {
+                pathOrder += getDistance(checkpoints[order[i - 1]], checkpoints[order[i]]);
+            }
+            return pathOrder;
         }
 
         private static double getDistance(Point p1, Point p2)
         {
             return Math.Sqrt((p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y));
+        }
+
+        private static void CheckPermutation(int[] permutation,int [] bestOrder, Point[] checkpoints)
+        {
+            int position = permutation.Length;
+            if (getPathOrder(permutation, checkpoints, position) <
+                    getPathOrder(bestOrder, checkpoints, position))
+            {
+                for (int i = 0; i < permutation.Length; i++)
+                    bestOrder[i] = permutation[i];
+            };
         }
     }
 }
