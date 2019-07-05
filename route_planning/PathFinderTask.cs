a30bb8b309ae88fba37f78c951d49321197a3101
Aleksey <aleks-sh@mail.ru>
2019-07-05 10:29:29 +0300

    2

--- route_planning/PathFinderTask.cs
+++ route_planning/PathFinderTask.cs
@@ -1,24 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace RoutePlanning
 {
-	public static class PathFinderTask
-	{
-		public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
-		{
-			var bestOrder = MakeTrivialPermutation(checkpoints.Length);
-			return bestOrder;
-		}
+    public static class PathFinderTask
+    {
+        public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
+        {
+            //var bestOrder = MakeTrivialPermutation(checkpoints.Length);
+            int[] permutation = new int[checkpoints.Length];
+            var bestOrder = MakePermutations(permutation, 1);
 
-		private static int[] MakeTrivialPermutation(int size)
-		{
-			var bestOrder = new int[size];
-			for (int i = 0; i < bestOrder.Length; i++)
-				bestOrder[i] = i;
-			return bestOrder;
-		}
+            return bestOrder;
+        }
+
+        private static int[] MakeTrivialPermutation(int size)
+        {
+            var bestOrder = new int[size];
+            for (int i = 0; i < bestOrder.Length; i++)
+                bestOrder[i] = i;
+            return bestOrder;
+        }
+
+        public static int[] MakePermutations(int[] permutation, int position)
+        {
+            // база рекурсии: позиция равна длине перестановки, достигнут последний элемент
+            if (position == permutation.Length)
+            {
+                return permutation;
+            }
+
+
+            //по очереди ставим все элементы на текущую позицию,
+            //если их еще нет в существующей перестановки в позициях от 0 до текущей
+            for (int i = 0; i < permutation.Length; i++)
+            {
+                bool found = false;
+                //если текущий элемент найден в перестановке в позиции от 0 до текущей,
+                //работаем со сл элементом
+                for (int j = 0; j < position; j++)
+                    if (permutation[j] == i)
+                    {
+                        found = true;
+                        break;
+                    }
+                if (found) continue;
+                //если елемента нет в перестановке, ставим его на текущее место
+                permutation[position] = i;
+                //вызов для сл позиции
+                MakePermutations(permutation, position + 1);
+            }
+        }
+
+
 
         private static double getDistance(Point p1, Point p2)
         {
