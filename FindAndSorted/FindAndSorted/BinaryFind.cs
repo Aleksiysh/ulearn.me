7c780da8b300324c118f571a18cf8fd1a7862716
Aleksey <aleks-sh@mail.ru>
2019-07-05 22:19:58 +0300

    kirilitsa

---
+++ FindAndSorted/FindAndSorted/BinaryFind.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAndSorted
+{
+    class BinaryFind
+    {
+        public static int Find(int[] array, int query)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            while (left < right)
+            {
+                var middle = (left + right) / 2;
+                if (query <= array[middle])
+                    right = middle;
+                else
+                    left = middle + 1;
+            }
+            if (array[right] == query)
+                return right;
+            return -1;
+        }
+    }
+}
---
+++ FindAndSorted/FindAndSorted/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAndSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( BinaryFind.Find(new int[] { 1, 2, 4, 5, 5, 5, 6, 7, 7, 8 }, 5));
+
+            Console.WriteLine("Press any key");
+            Console.Read();
+        }
+    }
+}
