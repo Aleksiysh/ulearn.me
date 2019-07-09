3a60b01e3f2baa334cee9aca5055c8045a5df45a
aleks-sh <aleks-sh@mail.ru>
2019-07-09 12:17:34 +0300

    Array исходный

--- MyArray/Array/Program.cs
+++ MyArray/Array/Program.cs
@@ -10,9 +10,39 @@ namespace MyArray
     {
         public static void Main()
         {
-            CombineArray.Run();
+            var intArr = new int[] { 2, 4, 6, 2, 6, 5 };
+            BubbleSort(intArr);
+            var strArr = new string[] { "A", "D", "B", "F", "C" };
+            BubbleSort(strArr);
 
-            
+
+
+        }
+
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = array.Length-1; i >= 0; i--)
+                for (int j = 1; j <= i; j++)
+                    if (array[j] < array[j - 1])
+                    {
+                        var t = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = t;
+                    }
+        }
+
+
+        public static void BubbleSort(string[] array)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+                for (int j = 1; j <= i; j++)
+                    if (array[j].CompareTo(array[j-1])<0)
+                    {
+                        var t = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = t;
+                    }
         }
     }
 }
