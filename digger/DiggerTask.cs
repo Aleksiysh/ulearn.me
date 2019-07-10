3334da73b8acb930574dddcdfb7f19ee8a089862
Aleksey <aleks-sh@mail.ru>
2019-07-10 04:12:07 +0300

    Digger 1

--- digger/DiggerTask.cs
+++ digger/DiggerTask.cs
@@ -7,6 +7,51 @@ using System.Threading.Tasks;
 namespace Digger
 {
     //Напишите здесь классы Player, Terrain и другие.
+    public class Terrain : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand();
+        }
 
-   
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 0; 
+        }
+
+        public string GetImageFileName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Player : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 0;
+        }
+
+        public string GetImageFileName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
 }
