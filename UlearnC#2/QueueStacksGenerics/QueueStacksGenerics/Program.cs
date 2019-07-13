4e61e57713781c6f3b45cac12e43c80b5671c378
Aleksey <aleks-sh@mail.ru>
2019-07-13 12:28:50 +0300

    Generic

--- UlearnC#2/QueueStacksGenerics/QueueStacksGenerics/Program.cs
+++ UlearnC#2/QueueStacksGenerics/QueueStacksGenerics/Program.cs
@@ -10,29 +10,48 @@ namespace QueueStacksGenerics
     {
         static void Main(string[] args)
         {
-            #region Стек Array
-            var n = 30;
-            var stack = new MyStackArr();
-            for (var i = 0; i < n; i++) stack.Push(i);
-            for (var i = 0; i < n; i++) Console.Write(stack.Pop() + " ");
+            #region Стек Очередь <int>
+            //var n = 30;
+            //var stack = new MyStackArr();
+            //for (var i = 0; i < n; i++) stack.Push(i);
+            //for (var i = 0; i < n; i++) Console.Write(stack.Pop() + " ");
 
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            var queue = new MyQueueArr();
-            for (var i = 0; i < n; i++) queue.Enqueue(i);
-            for (var i = 0; i < n; i++) Console.Write(queue.Dequeue() + " ");
+            //var queue = new MyQueueArr();
+            //for (var i = 0; i < n; i++) queue.Enqueue(i);
+            //for (var i = 0; i < n; i++) Console.Write(queue.Dequeue() + " ");
 
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            var queueLL = new QueueLinkList();
-            for (var i = 0; i < n; i++) queueLL.Enqueue(i);
-            for (var i = 0; i < n; i++) Console.Write(queueLL.Dequeue() + " ");
+            //var queueLL = new QueueLinkList();
+            //for (var i = 0; i < n; i++) queueLL.Enqueue(i);
+            //for (var i = 0; i < n; i++) Console.Write(queueLL.Dequeue() + " ");
 
             #endregion
 
+            #region Generic            
+            //var queueG = new QueueGeneric<string>();
+            //queueG.Enqueue("A");
+            //queueG.Enqueue("B");
+            //queueG.Enqueue("C");
+            //for (var i = 0; i < 3; i++) Console.Write(queueG.Dequeue() + " ");
 
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //var intQueue = new QueueGeneric<int>();
+            //intQueue.Enqueue(1);
+            //intQueue.Enqueue(2);
+            //intQueue.Enqueue(2);
+            ////intQueue.Enqueue("Oops");
+
+            //var sum = 0;
+            //while (!intQueue.IsEmpty) sum += intQueue.Dequeue();
+            //Console.WriteLine(sum);
+            #endregion
 
 
             Console.ReadKey();
---
+++ UlearnC#2/QueueStacksGenerics/QueueStacksGenerics/QueueGeneric.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueStacksGenerics
+{
+    class QueueGeneric<TValue>
+    {
+        QueueItemGeneric<TValue> head;
+        QueueItemGeneric<TValue> tail;
+        public bool IsEmpty { get { return head == null; } }
+
+        public void Enqueue(TValue value)
+        {
+            var item = new QueueItemGeneric<TValue> { Value = value };
+            if(head == null)
+            {
+                head = tail = item;
+            }
+            else
+            {
+                tail.Next = item;
+                tail = item;
+            }
+        }    
+
+        public TValue Dequeue()
+        {
+            if (head == null)
+                throw new InvalidOperationException();
+            var result = head.Value;
+            head = head.Next;
+            if (head == null) tail = null;
+            return result;
+        }
+    }
+
+    class QueueItemGeneric<TValue>
+    {
+        public TValue Value { get; set; }
+        public QueueItemGeneric<TValue> Next { get; set; }
+    }
+}
