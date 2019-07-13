77ec57964a89fdddd7872283c922574b54168f08
Aleksey <aleks-sh@mail.ru>
2019-07-13 11:51:55 +0300

    LinkList

--- UlearnC#2/QueueStacksGenerics/QueueStacksGenerics/Program.cs
+++ UlearnC#2/QueueStacksGenerics/QueueStacksGenerics/Program.cs
@@ -10,6 +10,32 @@ namespace QueueStacksGenerics
     {
         static void Main(string[] args)
         {
+            #region Стек Array
+            var n = 30;
+            var stack = new MyStackArr();
+            for (var i = 0; i < n; i++) stack.Push(i);
+            for (var i = 0; i < n; i++) Console.Write(stack.Pop() + " ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var queue = new MyQueueArr();
+            for (var i = 0; i < n; i++) queue.Enqueue(i);
+            for (var i = 0; i < n; i++) Console.Write(queue.Dequeue() + " ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var queueLL = new QueueLinkList();
+            for (var i = 0; i < n; i++) queueLL.Enqueue(i);
+            for (var i = 0; i < n; i++) Console.Write(queueLL.Dequeue() + " ");
+
+            #endregion
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ UlearnC#2/QueueStacksGenerics/QueueStacksGenerics/QueueLinkList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueStacksGenerics
+{
+    class QueueLinkList
+    {
+        MyQueueItem head;
+        MyQueueItem tail;
+
+        public void Enqueue(int value)
+        {
+            var item = new MyQueueItem { Value = value };
+            if(head == null)
+            {
+                head = tail = item;
+            }
+            else
+            {
+                tail.Next = item;
+                tail = item;
+            }
+        }    
+        public int Dequeue()
+        {
+            if (head == null)
+                throw new InvalidOperationException();
+            var result = head.Value;
+            head = head.Next;
+            if (head == null) tail = null;
+            return result;
+        }
+    }
+
+    class MyQueueItem
+    {
+        public int Value { get; set; }
+        public MyQueueItem Next { get; set; }
+    }
+}
